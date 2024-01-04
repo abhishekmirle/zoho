62296fa8aaa19d6d6e62f9cabde7f22e7f70d185
Abhi <Abhi@DESKTOP-O817363>
2024-01-04 21:48:30 +0530

    first commit

---
+++ Zoho/src/test/java/com/actitime/TestScripts/CustomerModule.java
@@ -0,0 +1,12 @@
+package com.actitime.TestScripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+	@Test
+	public void creatcustomer() {
+		Reporter.log("creatCustomer");
+	}
+
+}
